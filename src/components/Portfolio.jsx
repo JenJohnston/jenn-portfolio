2162f2ff92232c0b4182f86cd12c6b752f5d46d0
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-11 17:46:49 -0600

    .

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-
-import img1 from '../assets/images/portfolio1.jpg'
-import img2 from '../assets/images/portfolio2.jpg'
-import img3 from '../assets/images/portfolio3.jpg'
-import img4 from '../assets/images/portfolio4.jpg'
-import img5 from '../assets/images/portfolio5.png'
-import img6 from '../assets/images/portfolio6.jpg'
+import portfolioData from '../data/portfolioData'
 
 export default function Portfolio() {
     
@@ -17,66 +11,22 @@ export default function Portfolio() {
                 <h2>Web Portfolio</h2>
             </div>
             <div className="container portfolio__container">
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
-                <article className="portfolio__item">
-                    <div className="portfolio__img">
-                        <img src={img1} alt="" />
-                    </div>
-                    <h3>Portfolio Item Title</h3>
-                    <div className="portfolio__cta">
-                        <a href="https://github.com/JenJohnston" className='btn-std' >Github</a>
-                        <a href="https://magic-portal-demo.netlify.app/" className='btn-primary' >Live Demo</a>
-                    </div>
-                </article>
+                {
+                    portfolioData.map(({id, image, title, github, livedemo}) => {
+                        return (
+                            <article className="portfolio__item">
+                                <div className="portfolio__img">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__cta">
+                                    <a href={github} className='btn-std' >Github</a>
+                                    <a href={livedemo} className='btn-primary'>Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
---
+++ src/data/portfolioData.js
@@ -0,0 +1,53 @@
+import img1 from '../assets/images/portfolio1.jpg'
+import img2 from '../assets/images/portfolio2.jpg'
+import img3 from '../assets/images/portfolio3.jpg'
+import img4 from '../assets/images/portfolio4.jpg'
+import img5 from '../assets/images/portfolio5.png'
+import img6 from '../assets/images/portfolio6.jpg'
+
+const portfolioData = [
+    {
+        id:1,
+        image: img1,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    },
+    {
+        id:2,
+        image: img2,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    },
+    {
+        id:3,
+        image: img3,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    },
+    {
+        id:4,
+        image: img4,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    },
+    {
+        id:5,
+        image: img5,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    },
+    {
+        id:6,
+        image: img6,
+        title: "Threejs Journey Magical Portal",
+        github: "https://github.com/JenJohnston/magic-portal",
+        livedemo: "https://magic-portal-demo.netlify.app"
+    }
+]
+
+export default portfolioData
