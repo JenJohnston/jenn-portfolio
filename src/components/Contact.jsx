38f3a9c826e050eb5cb426f3c622fe94802d4816
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-05-11 21:45:51 -0600

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
+import { HiOutlineMail } from 'react-icons/hi'
+import { RiMessengerLine } from 'react-icons/ri'
+import { RiWhatsappLine } from 'react-icons/ri'
 export default function Contact() {
     
+    const email = 'mailto:jen.johnston@protonmail.ch'
 
     return (
         <section id="contact">
-            <h2>Contact</h2>
+            <div className="section__header">
+                <h5>Get In Touch</h5>
+                <h2>Contact Me</h2>
+            </div>
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <h4>Email</h4>
+                        <h5>jen.johnston@protonmail.ch</h5>
+                        <a href={email} ></a>
+                    </article>
+                </div>
+                <form action="POST">
+
+                </form>
+            </div>
         </section>
     )
 }
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
 import testimonialData from '../data/testimonialData'
 
 
@@ -12,19 +26,25 @@ export default function Testimonials() {
                 <h5>Client Feeback</h5>
                 <h2>Testimonials</h2>
             </div>
-            <div className="container testimonials__container">
-                {testimonialData.map(({id, image, name, review}) => {
+            <Swiper 
+                className="container testimonials__container"
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+            >
+                {testimonialData.map(({image, name, review}, index) => {
                     return (
-                        <article className="testimonial" key={id}>
+                        <SwiperSlide className="testimonial" key={index}>
                             <div className="client__img">
                                 <img src={image} alt="" />
                             </div>
                             <h5 className='client-name'>{name}</h5>
                             <small className='client-review'>{review}</small>
-                        </article>
+                        </SwiperSlide>
                     )
                 })}
-            </div>
+            </Swiper>
         </section>
     )
 }
